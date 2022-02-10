72af96ac89ed8acde26241fb4d246faa3bf5d386
iamkingclutch3 <69503046+iamkingclutch3@users.noreply.github.com>
2022-02-10 17:43:50 +0100

    fix config

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 let config = {
-    token: "OTQxMDEwMjkxMjk3MjM5MDYw.YgPuIA.60L5y6tUhsHLDSxJcDIu-5YX9oM",
+    token: "OTQxMDk0Njk5NDk2NzY3NDk4.YgQ8vQ.hBPlC_B_M6t1wQsP9RNWAX6f2Io",
     //kareem: OTQxMDk0Njk5NDk2NzY3NDk4.YgQ8vQ.hBPlC_B_M6t1wQsP9RNWAX6f2Io
     //tests: OTQxMDEwMjkxMjk3MjM5MDYw.YgPuIA.60L5y6tUhsHLDSxJcDIu-5YX9oM
-    clientId: "941010291297239060",
+    clientId: "941094699496767498",
     //official: 941094699496767498
     //tests: 941010291297239060
     admin: "914297695479398421",
     allowrole: "860550457214500884",
-    serverId: "859394328577310742",
+    serverId: "914255296677965875",
     //panthers: 914255296677965875
     //tests: 859394328577310742
     prefix: "!wallet",
-    logchannel: "868043397280174090"
+    logchannel: "914555514082435132"
     //panthers: 914555514082435132
     //tests: 868043397280174090
 }
