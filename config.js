4513c3f0dc0d59fb9df8d9c9618d52c68ab2bab2
iamkingclutch3 <69503046+iamkingclutch3@users.noreply.github.com>
2024-01-14 19:12:53 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,11 @@
 let config = {
-    token: "OTQxMDk0Njk5NDk2NzY3NDk4.YgQ8vQ.hBPlC_B_M6t1wQsP9RNWAX6f2Io",
-    //kareem: OTQxMDk0Njk5NDk2NzY3NDk4.YgQ8vQ.hBPlC_B_M6t1wQsP9RNWAX6f2Io
-    //tests: OTQxMDEwMjkxMjk3MjM5MDYw.YgPuIA.60L5y6tUhsHLDSxJcDIu-5YX9oM
-    clientId: "941094699496767498",
-    //official: 941094699496767498
-    //tests: 941010291297239060
-    admin: "914297695479398421",    
-    allowrole: "932644288611885086",
-    serverId: "914255296677965875",
-    //panthers: 914255296677965875
-    //tests: 859394328577310742
+    token: "",
+    clientId: "",
+    admin: "",    
+    allowrole: "",
+    serverId: "",
     prefix: "!wallet",
-    logchannel: "914555514082435132"
-    //panthers: 914555514082435132
-    //tests: 868043397280174090
+    logchannel: ""
 }
 
 module.exports = config
